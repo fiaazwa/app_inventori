18d4cabdf615b0b8524708f1e315f8981376138d
fiaazw <rrofia5@gmail.com>
2024-06-10 15:37:50 +0900

    setup project

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+header('location: ./src/index.php');
+
---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Azrn_Halaman Awal</title>
+    <link rel="stylesheet" href="./../.css/output.css">
+</head>
+<body>
+    <h1 class="text-red-700">hello</h1>
+</body>
+</html>
